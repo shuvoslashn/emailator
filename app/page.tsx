661db3a99375ddbb0613602697a340ae9d742528
Shuvo Sarker <sarker3664@gmail.com>
2025-01-29 08:12:51 +0600

    docs: nextjs 15 installed with shadcn ui

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <section className="w-full h-screen flex justify-center items-center">
+            <h1 className="text-8xl text-center font-bold">Emailator</h1>
+        </section>
+    );
+}
